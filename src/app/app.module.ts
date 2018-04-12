04d8bc364cfee80a88c18b209959242a45843b7f
Milad Vafaeifard <milad_vafaeifard@epam.com>
2018-04-12 10:51:58 +0200

    clear up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,22 @@ import { Logger } from './logger/logger.service';
 import { NewLogger } from './logger/newLogger.service';
 import { OtherLogger } from './logger/otherLogger.service';
 import { AppComponent } from './app.component';
-import { FooComponent } from './foo.component';
 
 @NgModule( {
-    declarations: [ AppComponent, FooComponent ],
+    declarations: [ AppComponent ],
     imports: [ BrowserModule ],
     providers: [
-        NewLogger,
-        {
-            provide: Logger,
-            useClass: NewLogger
-        },
-        {
-            provide: OtherLogger,
-            useClass: NewLogger
-        }
+        // NewLogger,
+        Logger,
+        OtherLogger,
+        // {
+        //     provide: Logger,
+        //     useClass: NewLogger
+        // },
+        // {
+        //     provide: OtherLogger,
+        //     useClass: NewLogger
+        // }
     ],
     bootstrap: [ AppComponent ]
 } )
